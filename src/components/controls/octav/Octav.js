aa91e14eb7d218baaad8a5877c1a8bc00589df3b
sea light <sigalh7@gmail.com>
2020-12-12 13:06:43 +0200

    functionality+function change

--- src/components/controls/octav/Octav.js
+++ src/components/controls/octav/Octav.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 
 const Octav = () => {
+
+	const [ octav, setOctav ] = useState(0)
+
+	const octavChange = (octavKey) => {
+		let tempCount = octav;
+		if ( octav < 3 && octavKey === "up") {
+			tempCount++ ;
+		} 
+		if ( octav > -3 && octavKey === "dn") {
+			tempCount--;
+		}
+		setOctav(tempCount)
+	}
+
 	return (
 		<div className="octav centerIt flexColumn">
 			<div className="octav-container control-container centerIt">
 				<div className="centerIt flexColumn">
-					<button className="octaveDn" onClick="octavChange('dn')">{`<`}</button>
-					<label htmlFor="octaveDn">Dn</label>
+					<button className="octaveDn" onClick={() => {octavChange('dn')}}>{`<`}</button>
+					<p>Dn</p>
 				</div>
 				<div className="octav-text-container">
-					<p className="octav-text">0</p>
+					<p className="octav-text">{octav}</p>
 				</div>
 				<div className="centerIt flexColumn">
-					<button className="octaveUp" onClick="octavChange('up')">{`>`}</button>
-					<label htmlFor="octaveUp">Up</label>
+					<button className="octaveUp" onClick={() => {octavChange('up')}}>{`>`}</button>
+					<p>Up</p>
 				</div>
 			</div>
 			<p>
